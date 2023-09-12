052eb412e9e5d91576012df8774cc59f8a2242f0
mariohany98 <mariohany98@gmail.com>
2023-09-13 01:07:43 +0300

    add file

---
+++ backup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Print-green function
+print_green(){
+	GREEN="\033[0;32m"
+	NC="\033[0m"
+	echo  "${GREEN} $1 ${NC}"
+}
+
+# Create the directory where backups will be stored
+if [ -d /backup ]
+then
+	continue
+else
+	print_green "/backup directory has been created"
+	mkdir /backup
+fi
+
+# What to backup.
+backup_files="/share1"
+
+# where to store the backup.
+backup_store="/backup"
+
+# Name of archive file.
+day=`date +%F`
+hostname=`hostname`
+archive_file="$hostname-$day.tar.gzip"
+
+# Start status message.
+print_green "Backing up $backup_files to $backup_store/$archive_file"
+
+# Backup the files using tar.
+tar czpf $backup_store/$archive_file $backup_files
+
+# End status message 
+print_green "Backup finished"
